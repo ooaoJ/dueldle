b0c521d64689f6d1a5d050272998458016f8fae0
Cesar Augusto <cesaraugusto23121989@gmail.com>
2025-08-27 22:36:25 -0300

    add cards route

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,14 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\CardController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::resource('cards', CardController::class)->middleware('auth');
+
+Route::get('/home',function(){
+    return view('admin.home');
+})->name('home')->middleware('auth');
