9dbf1839a3610367976bb2aa9f6ef8803cc192a8
Cesar Augusto <cesaraugusto23121989@gmail.com>
2025-09-01 19:58:41 -0300

    commit add route card validator

--- routes/api.php
+++ routes/api.php
@@ -3,9 +3,12 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ClassicModeController;
+use Illuminate\Support\Facades\Cache;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
 Route::post('/classic/query',[ClassicModeController::class,'query'])->name('classic.query');
+
+Route::post('/classic/validate/card',[ClassicModeController::class,'cardValidator'])->name('classic.validate');
