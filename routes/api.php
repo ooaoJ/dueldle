797b097337941c017dce87772108281930f7d4f6
Cesar Augusto <cesaraugusto23121989@gmail.com>
2025-08-31 23:29:59 -0300

    commit add route(return route of cards)

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,10 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ClassicModeController;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+Route::post('/classic/query',[ClassicModeController::class,'query'])->name('classic.query');
